bb1522fdf41b111a0776224621ce4d5c579f9bc9
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2022-06-04 11:07:28 +0000

    update header & footer menu

--- src/components/shared/menu.ts
+++ src/components/shared/menu.ts
@@ -1,7 +1,13 @@
 const menus = [
+  { name: 'Layanan', url: '/layanan' },
   { name: 'Karya', url: '/karya' },
   { name: 'Konsep', url: '/konsep' },
   { name: 'Kontak', url: '/kontak' },
+]
+
+export const footerMenu = [
+  { name: 'Kebijakan Privasi', url: '/privacy' },
+  { name: 'Informasi Legalitas', url: '/legal' },
 ]
 
 export default menus
